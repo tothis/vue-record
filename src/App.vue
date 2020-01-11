92d92754c38349a7ce7f5158a27b9f36b59d8ebb
lilei <tothis@qq.com>
2020-01-11 14:09:55 +0800

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,28 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/header">header</router-link> |
-      <router-link to="/user">user</router-link> |
+      <router-link to="/">Home</router-link>
+      |
+      <router-link to="/about">About</router-link>
+      |
+      <router-link to="/header">header</router-link>
+      |
+      <router-link to="/user">user</router-link>
+      |
       <router-link to="/file-upload">file-upload</router-link>
+      |
+      <router-link to="/data-table">table</router-link>
     </div>
-    <router-view/>
+    <router-view></router-view>
   </div>
 </template>
 
 <style lang="stylus">
 #app
-  font-family 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+    font-family 'Avenir', Helvetica, Arial, sans-serif
+    -webkit-font-smoothing antialiased
+    -moz-osx-font-smoothing grayscale
+    text-align center
+    color #2c3e50
+    margin-top 60px
 </style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -5,37 +5,42 @@ import Home from "@/views/Home.vue";
 Vue.use(VueRouter);
 
 const routes = [
-    {
-        path: "/",
-        name: "home",
-        component: Home
-    },
-    {
-        path: "/about",
-        name: "about",
-        component: () => import("@/views/About.vue")
-    },
-    {
-        path: "/header",
-        name: "header",
-        component: () => import('@/components/header.vue')
-    },
-    {
-        path: "/user",
-        name: "user",
-        component: () => import('@/components/user.vue')
-    },
-    {
-        path: "/file-upload",
-        name: "file-upload",
-        component: () => import('@/components/file-upload/index.vue')
-    }
+  {
+    path: "/",
+    name: "home",
+    component: Home
+  },
+  {
+    path: "/about",
+    name: "about",
+    component: () => import("@/views/About.vue")
+  },
+  {
+    path: "/header",
+    name: "header",
+    component: () => import("@/components/header.vue")
+  },
+  {
+    path: "/user",
+    name: "user",
+    component: () => import("@/components/user.vue")
+  },
+  {
+    path: "/file-upload",
+    name: "file-upload",
+    component: () => import("@/components/file-upload/index.vue")
+  },
+  {
+    path: "/data-table",
+    name: "data-table",
+    component: () => import("@/views/data-table.vue")
+  }
 ];
 
 const router = new VueRouter({
-    mode: "history",
-    base: process.env.BASE_URL,
-    routes
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes
 });
 
 export default router;
