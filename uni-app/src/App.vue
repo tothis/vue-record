65d90e3bbca4fede16f834716d0416c388fd91ce
lilei <tothis@qq.com>
2020-06-11 16:27:07 +0800

    initial

---
+++ uni-app/src/App.vue
@@ -0,0 +1,17 @@
+<script>
+	export default {
+        onLaunch: function() {
+			console.log('app launch')
+		},
+        onShow: function() {
+			console.log('app show')
+		},
+        onHide: function() {
+			console.log('app hide')
+		}
+	}
+</script>
+
+<style>
+	/* 每个页面公共css */
+</style>
